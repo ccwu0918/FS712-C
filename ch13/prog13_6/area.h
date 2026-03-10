c72e0c86dd741cac5dfa2f8d4143776319676d28
Chia-Chun Wu <ccwu0918@gmail.com>
2026-03-10 15:32:04 +0800

    Update area.h

--- ch13/prog13_6/area.h
+++ ch13/prog13_6/area.h
@@ -1,7 +1,8 @@
-/* area.h 的標題檔*/
+/* area.h 的標頭檔 */
 #define PI 3.14159
 #define CIRCLE(r) ((PI)*(r)*(r))
 #define RECTANGLE(length,height) ((length)*(height))
 #define TRIANGLE(base,height) ((base)*(height)/2.)
+
 
 
